3d8be3d4093aaa17511eabf052041ccd36f91e8c
klementj <klementj@gmail.com>
2020-12-15 21:49:19 +0100

    Add component to list resources

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <Header title="Remember Me" />
+  <ResourceList />
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import ResourceList from './components/ResourceList.vue'
 
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    ResourceList
   }
 }
 </script>
