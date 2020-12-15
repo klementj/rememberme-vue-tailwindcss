3945c74d3890a64b6af0aa29c9dbe19049f7c547
klementj <klementj@gmail.com>
2020-12-15 21:54:27 +0100

    Add button below list of resources

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <Header title="Remember Me" />
   <ResourceList />
+  <button class="rounded-lg w-auto">
+    <a href="#" class="hover:text-gray-500 hover:bg-blue-50 hover:border-blue-200 transition-colors w-full flex items-center justify-center rounded-lg border-2 border-dashed border-blue-100 text-sm font-medium py-5 px-3 text-gray-400"
+    >
+      Add resource
+    </a>
+  </button>
 </template>
 
 <script>
