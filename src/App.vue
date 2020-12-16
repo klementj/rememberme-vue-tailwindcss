c85e705ee711ea7f9d7be5137924ef306947aa1e
klementj <klementj@gmail.com>
2020-12-16 09:38:02 +0100

    Fix: give add-resource-button full width

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <Header title="Remember Me" />
   <ResourceList />
-  <button class="rounded-lg w-auto">
+  <button class="rounded-lg w-full">
     <a href="#" class="hover:text-gray-500 hover:bg-blue-50 hover:border-blue-200 transition-colors w-full flex items-center justify-center rounded-lg border-2 border-dashed border-blue-100 text-sm font-medium py-5 px-3 text-gray-400"
     >
       Add resource
